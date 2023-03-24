a4ba10698609cb10c4413bf9020afe5f66fdedbd
francisamidu <francisamidu124@gmail.com>
2023-03-24 20:26:37 +0200

    Make code and text completion request to OpenAIAPI

--- src/api/services/apiService.ts
+++ src/api/services/apiService.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import { Configuration, OpenAIApi } from "openai";
+import { validationResult } from "express-validator";
+import { errorFormatter } from "../utils";
 const ck = require("ckey");
 
 const configuration = new Configuration({
@@ -13,8 +15,42 @@ class ApiService {
       message: "Welcome to anime scraper",
     });
   }
-  static async complete(req: Request, res: Response) {}
+  static async complete(req: Request, res: Response) {
+    const result = validationResult(req).formatWith(errorFormatter);
 
-  static async translate(req: Request, res: Response) {}
+    if (!result.isEmpty()) {
+      return res.json({ errors: result.array() });
+    }
+
+    const { prompt } = req.body;
+
+    const code = await openaiApi.createCompletion({
+      model: "text-davinci-003",
+      prompt,
+    });
+
+    res.status(200).json({
+      result: code.data.choices[0].text,
+    });
+  }
+
+  static async translate(req: Request, res: Response) {
+    const result = validationResult(req).formatWith(errorFormatter);
+
+    if (!result.isEmpty()) {
+      return res.json({ errors: result.array() });
+    }
+
+    const { prompt } = req.body;
+
+    const code = await openaiApi.createCompletion({
+      model: "code-davinci-002",
+      prompt,
+    });
+
+    res.status(200).json({
+      result: code.data.choices[0].text,
+    });
+  }
 }
 export default ApiService;
